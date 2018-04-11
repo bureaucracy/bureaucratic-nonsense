9e0ad7c1187ea2936140233cdf6a65f994e86cb6
Jen <error@error.media>
2018-04-11 06:32:34 -0700

    more js updates

--- javascript-summary.js
+++ javascript-summary.js
@@ -41,6 +41,14 @@ ex2.printAll.apply({
 
 // currying
 
+function currySum(x) {
+  return function (y) {
+    return x + y
+  }
+}
+
+currySum(1)(2)          // outputs: 3
+
 // const, let
 
 const CANNOT_REASSIGN = true // can also be const cannotReassign = true
@@ -97,16 +105,17 @@ doACallPr(false)
 // async/await
 
 const doACallAs = async function (pass = true) {
-  return new Promise((resolve, reject) => {
-    if (!pass) {
-      console.log(false)
-      return reject(false)
-    }
+  if (!pass) {
+    console.log(false)
+    throw false
+  }
 
+  setTimeout(() => {
     console.log(true)
-    return resolve(true)
-  })
+    return true
+  }, 1)
 }
+
 
 await doACallAs()
 
@@ -118,10 +127,80 @@ try {
 
 // map, filter, reduce
 
+const arr = ['a', 'b', 'c']
+
+arr.map((item) => {
+  return `${item} is called`
+})        // map outputs: ["a is called", "b is called", "c is called"]
+
+arr.filter((item) => {
+  return item < 'c'
+})        // filter outputs: ["a", "b"]
+
+arr.reduce((lastItem, item) => {
+  return lastItem + item
+})        // reduce outputs: abc
+
 // classes
+
+class someClass {
+  constructor(alpha) {
+    this._alpha = alpha || true
+    this._beta = false
+    this._gamma = 1
+  }
+
+  get beta() {
+    return this._beta
+  }
+
+  get gamma() {
+    return this._gamma
+  }
+
+  get alpha() {
+    return this._alpha
+  }
+
+  set gamma(val) {
+    this._gamma = val
+  }
+
+  printAll() {
+    console.log(this._alpha, this._beta, this._gamma)
+  }
+}
+
+const sc = new someClass()
+
+sc.beta                     // outputs: false
+sc.gamma = true; sc.gamma   // outputs: true
+sc.printAll()               // outputs: true, false, true
+
+const sc2 = new someClass(2)
+
+sc2.alpha                 // outputs: 2
 
 // destructuring
 
+[a, b, ...rest] = ['a', 'b', 'c', 'd', 'e']
+
+console.log(rest)         // outputs: ['c', 'd', 'e']
+
 // object spread (opposite of rest parameters)
 
+function spread(a, b, c) {
+  return `${a} then ${b} then ${c}`
+}
+
+const colors = ['red', 'green', 'blue']
+
+console.log(spread(...colors))   // outputs: red then green then blue
+
 // rest parameters (opposite of object spread)
+
+function rest(...args) {
+  console.log(...args)
+}
+
+rest('a', 'b', 'c')             // outputs: a b c
